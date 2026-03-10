16ff8c93146c01495a8f9b6293671241fc37f3f7
YTSHORT <ytshort@gmail.com>
2026-03-10 12:38:06 +0500

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
